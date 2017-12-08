113506fbaeec43c6e3cdae80f5ad7598e97c77e2
nicolettee <nicolette.taylor@gmail.com>
2017-12-08 16:47:57 -0600

    fixed-ish

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,11 @@
 
-function myFunction() {
-    var x = document.getElementById("myTopNav");
-    
-        x.classList.toggle("responsive");
-    
-}
+var productList2 = [
+    {product: 'Silk Crepe Print 1920s Dress', price: 150.00, color:'Blue'},
+    {product: 'Pants', price: 10.00, color:'Brown'},
+    {product: 'Shoes', price: 15.00, color: 'Blue'}
+];
+
+var filtered = productList2.filter(function(x){
+  return (x.product == 'Pants')
+});
+console.log(filtered);
