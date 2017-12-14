32586b928053852c67943a69c710e6556895068e
nicolettee <nicolette.taylor@gmail.com>
2017-12-14 15:21:27 -0600

    items

--- scripts/main.js
+++ scripts/main.js
@@ -1,58 +1,34 @@
-var productList2 = [{
-    product: 'Silk Crepe Print 1920s Dress',
-    price: 150.00,
-    color: 'Blue'
-  },
-  {
-    product: 'Pants',
-    price: 10.00,
-    color: 'Brown'
-  },
-  {
-    product: 'Shoes',
-    price: 15.00,
-    color: 'Blue'
+function storageAvailable(type) {
+  try {
+      const storage = window[type],
+          x = '__storage_test__';
+      storage.setItem(x, x);
+      storage.removeItem(x);
+      return true;
   }
-];
-
-var filtered = productList2.filter(function (x) {
-  return (x.product == 'Pants')
-});
-
-$(".btn-cart").on("click", function () {
-  
-  var myItems = getCookie("items");
-
-  if (myItems == null) {
-    myItems = [];
-  }else{
-    myItems = myItems.split(',');
+  catch(e) {
+      return e instanceof DOMException && (
+          // everything except Firefox
+          e.code === 22 ||
+          // Firefox
+          e.code === 1014 ||
+          // test name field too, because code might not be present
+          // everything except Firefox
+          e.name === 'QuotaExceededError' ||
+          // Firefox
+          e.name === 'NS_ERROR_DOM_QUOTA_REACHED') &&
+          // acknowledge QuotaExceededError only if there's something already stored
+          storage.length !== 0;
   }
-
-  myItems.push($(this).data("itemid"));
-  setCookie("items", myItems.join(','), 9999);
-
-});
-
-function setCookie(key,value,times){
-  var date=new Date();
-  date.setTime(date.getTime()+times*1000); 
-  document.cookie=key+"="+value+'; path=/';
+}
+if (storageAvailable('localStorage')) {
+ console.log("you can use local");// Yippee! We can use localStorage awesomeness
+}
+else {
+  // Too bad, no localStorage for us
 }
 
-function getCookie(key){
-  var scookies=document.cookie;result=null;
-  if(scookies){
-      var acookies=scookies.split(";");
-      acookies.map(function(item){
-          if(item.split("=")[0]===key){
-              result=item.split("=")[1];    
-          }
-      })
-  }
-  return result;
-}
 
-function removeCookie(key){
-  setCookie(key,0,-10);
-}
+storageAvailable('sessionStorage');
+
+
