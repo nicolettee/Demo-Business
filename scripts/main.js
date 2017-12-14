7cf3ff63022f92085e8fb1777352e74749b2f2f8
Daniel Worthy <daniel.worthy@rockfishinteractive.com>
2017-12-14 15:47:49 -0600

    Adding local storage usage

--- scripts/main.js
+++ scripts/main.js
@@ -22,13 +22,52 @@ function storageAvailable(type) {
   }
 }
 if (storageAvailable('localStorage')) {
- console.log("you can use local");// Yippee! We can use localStorage awesomeness
+     console.log("you can use local");// Yippee! We can use localStorage awesomeness
+    initializePage();
 }
 else {
   // Too bad, no localStorage for us
+  // Maybe hide all buttons
 }
 
 
-storageAvailable('sessionStorage');
+//storageAvailable('sessionStorage');
+
+function initializePage(){
+    //Declare our storage key to make typing easier
+    const storageKey = 'cart-key';
+
+
+    // Logging to see if we have anything in storage;
+    console.log(localStorage.getItem(storageKey));
+
+    //Add btn click event to add btns
+    $('.btn-cart').on('click', function(){
+        //get the id for the current cart item
+        let cartItemId = $(this).data('itemid');
+
+        //Retrieve stored cart  and split by comma
+        let currentCart = [];
+
+        // Store the current local storage in a temp variable
+        let storedCart = localStorage.getItem(storageKey);
+
+
+        //if stored cart is something, use it
+        if (storedCart != null){
+            currentCart = storedCart.split(',');
+        }
+
+        //add the new item to the cart
+        currentCart.push(cartItemId);
+
+        //update Storage
+        localStorage.setItem(storageKey, currentCart);
+        
+
+        //Update visible cart count
+    });
+
+}
 
 
